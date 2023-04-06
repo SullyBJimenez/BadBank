0b48bf47957d98d078ba959e0b0d95d4feabdedc
sullybjimenez <sullybjimenez@gmail.com>
2023-04-05 21:26:56 -0400

    updated h1 tags in withdraw componant and added balance componant

--- balance.js
+++ balance.js
@@ -1,8 +1,9 @@
 function Balance(){
+    const [balance, setBalance] = React.useState(0);
     const ctx = React.useContext(UserContext);
     return (
-        <h1>Balance<br/>
-            {JSON.stringify(ctx)}
+        <h1>Your current balance is:<br/>
+            {JSON.stringify(balance)}
         </h1>
     )
 }
--- withdraw.js
+++ withdraw.js
@@ -1,8 +1,8 @@
 function Withdraw(){
     const ctx = React.useContext(UserContext);
     return (
-        <h1>Withdraw<br/>
-            {JSON.stringify(ctx)}
+        <h1>Withdraw from your account<br/>
+            <Balance/>
         </h1>
     )
 }
